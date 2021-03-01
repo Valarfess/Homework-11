eb50050eb3807f89a8a0eb6e366756758db0a583
valarfess <valarfess@gmail.com>
2021-03-01 18:21:47 +0300

    Домашка 11

--- src/academy/belhard/AddressConnector.java
+++ src/academy/belhard/AddressConnector.java
@@ -9,12 +9,11 @@ public class AddressConnector {
     private static final String SELECT_ALL = "SELECT * FROM addresses";
     private static final String ADD = "INSERT INTO addresses (id, city, street, house_number, house_part, apartment_number) VALUES(?, ?, ?, ?, ?, ?)";
     private static final String DELETE = "DELETE FROM addresses where id = ?";
-    private static final String UPDATE = "UPDATE address SET city = ?, street = ?, house_number = ?, house_part = ?, apartment_number = ? WHERE id = ?";
+    private static final String UPDATE = "UPDATE addresses SET city = ?, street = ?, house_number = ?, house_part = ?, apartment_number = ? WHERE id = ?";
 
 
-    public static void add (Address address) {
+    public static void add(Address address) {
         Connection connection = DbConnectionProvider.getConnection();
-
         try (
                 PreparedStatement statement = connection.prepareStatement(ADD)
         ) {
@@ -32,75 +31,54 @@ public class AddressConnector {
         }
     }
 
-    public static List<Address> readAll(){
-
-            List<Address> res = new ArrayList<>();
-
-            Connection connection = DbConnectionProvider.getConnection();
-
-            try (PreparedStatement statement = connection.prepareStatement(SELECT_ALL)) {
-
-                ResultSet result = statement.executeQuery();
-
-                while (result.next()) {
-                    Address address = AddressUtil.toObject(result);
-
-                    res.add(address);
-                }
-
-
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-
-            return res;
-        }
-
-
-    public static void delete(int id){
+    public static List<Address> readAll() {
+        List<Address> res = new ArrayList<>();
 
         Connection connection = DbConnectionProvider.getConnection();
+        try (PreparedStatement statement = connection.prepareStatement(SELECT_ALL)) {
+            ResultSet result = statement.executeQuery();
+            while (result.next()) {
+                Address address = AddressUtil.toObject(result);
+                res.add(address);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
 
+    public static void delete(int id) {
+
+        Connection connection = DbConnectionProvider.getConnection();
         try (PreparedStatement statement = connection.prepareStatement(DELETE)) {
             connection.createStatement();
-        if (id == 1) {
-            statement.executeUpdate("DELETE FROM addresses where id = 1");
-        } else if (id == 2) {
-            statement.executeUpdate("DELETE FROM addresses where id = 2");
-        } else if (id == 3) {
-            statement.executeUpdate("DELETE FROM addresses where id = 3");
-        }
-        else if (id == 4) {
-            statement.executeUpdate("DELETE FROM addresses where id = 4");
-        }
-        else if (id == 5) {
-            statement.executeUpdate("DELETE FROM addresses where id = 5");
-        }
 
-        } catch(Exception ex){
+            int i = id;
+            statement.executeUpdate("DELETE FROM addresses where id = " + i);
+
+        } catch (Exception ex) {
             System.out.println();
         }
     }
-    public static void update (Address address){
-        Connection connection = DbConnectionProvider.getConnection();
 
+    public static void update(Address address, int id) {
+
+        Connection connection = DbConnectionProvider.getConnection();
 
         try (PreparedStatement statement = connection.prepareStatement(UPDATE)) {
             connection.createStatement();
 
-            statement.setString(1, "Brest");
-            statement.setString(2, "Zmagarou");
-            statement.setInt(3, 30);
-            statement.setInt(4, 5);
-            statement.setInt(5, 16);
-            statement.setInt(6, 1);
+            statement.setString(1, address.getCity());
+            statement.setString(2, address.getStreet());
+            statement.setInt(3, address.getHouseNumber());
+            statement.setInt(4, address.getHousePart());
+            statement.setInt(5, address.getApartmentNumber());
+            statement.setInt(6, id);
 
-            statement.executeUpdate(UPDATE);
-    }
+            statement.executeUpdate();
 
-         catch(Exception ex){
-            System.out.println();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-
     }
 }
--- src/academy/belhard/Main.java
+++ src/academy/belhard/Main.java
@@ -1,5 +1,7 @@
 package academy.belhard;
 
+import com.mysql.cj.x.protobuf.MysqlxCrud;
+
 import java.sql.Statement;
 
 public class Main {
@@ -12,21 +14,21 @@ public class Main {
         Address address4 = new Address(4,"Zaslavl","Odoevskogo",16,5,99);
         Address address5 = new Address(5,"Grodno","Belskogo",40,3,25);
 
-//        AddressConnector.add(address1);
-//        AddressConnector.add(address2);
-//        AddressConnector.add(address3);
-//        AddressConnector.add(address4);
-//        AddressConnector.add(address5);
+        AddressConnector.add(address1);
+        AddressConnector.add(address2);
+        AddressConnector.add(address3);
+        AddressConnector.add(address4);
+        AddressConnector.add(address5);
 
         System.out.println(AddressConnector.readAll());
 
-        AddressConnector.update(address1);
-        AddressConnector.update(address2);
+        AddressConnector.update(Address.createFoSaving("NewYork", "Lomonosova", 11, 2,101), 3);
+        AddressConnector.update(Address.createFoSaving("Moskov", "Kenedy",55, 4, 98),4);
         System.out.println(AddressConnector.readAll());
 
-//        AddressConnector.delete(3);
-//        AddressConnector.delete(4);
-//        System.out.println(AddressConnector.readAll());
+        AddressConnector.delete(1);
+        AddressConnector.delete(5);
+        System.out.println(AddressConnector.readAll());
 
     }
 }
